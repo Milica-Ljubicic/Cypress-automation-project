da35549beec35559a3f01d31a5a93a455507f8fe
Milica Ljubicic <milicaljubicic@Milicas-MacBook-Pro.local>
2023-11-15 14:41:13 +0100

    update

--- cypress/E2E/test1.cy.js
+++ cypress/E2E/test1.cy.js
@@ -1,8 +1,5 @@
 describe('Visit site', () => {
     it('should visit the website', () => {
       cy.visit('https://magento.softwaretestingboard.com/');
-  
-      cy.get('h1').should('contain', 'Magento Commerce');
-    });
   });
-  
+}) 
