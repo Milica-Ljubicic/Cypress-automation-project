ffda709b40f82d1be2d0c88a3c02e06d72eea9d8
Milica Ljubicic <milicaljubicic@192.168.1.7>
2023-11-16 22:18:30 +0100

    final

--- cypress.config.js
+++ cypress.config.js
@@ -2,7 +2,7 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
-    projectId: '35mp4n',
+    projectId: 'zf556p',
     baseUrl: 'https://magento.softwaretestingboard.com/',
     defaultCommandTimeout: 100000,
     setupNodeEvents(on, config) {
